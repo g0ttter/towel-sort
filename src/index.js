bc705fdb48cedc4573dac78b8d33b55764d78f35
g0ttter <kyka32@gmail.com>
2023-02-25 23:44:33 +0300

    add sort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  if(typeof matrix === 'undefinied' || !matrix || matrix.length < 1)
+    return [];
+  for(let i = 0; i < matrix.length; i++) {
+    if(i % 2 !== 0 && i != matrix.length)
+      result += matrix[i].reverse();
+    else 
+      result += matrix[i];
+    if(i !== matrix.length - 1)
+      result += ','
+  }
+  return result.split(',');
 }
